9ce23995bb8810f2725efcf97d18c9574fd48157
Mark Lavin <mlavin@caktusgroup.com>
2013-10-18 14:34:16 -0400

    Minimal Tic Tac Toe game.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,73 @@
 var servers = {"iceServers": [{"url": "stun:stun.l.google.com:19302"}]},
     config = {optional: [{RtpDataChannels: true}]};
 
+var GameView = Backbone.View.extend({
+    el: '#game',
+    events: {
+        "click .box": "pickBox"
+    },
+    initialize: function (options) {
+        this.leader = options.leader;
+        this.channel = options.channel;
+        this.channel.onmessage = _.bind(this.gotMessage, this);
+        this.piece = this.leader ? '#CD0000' : '#00688B';
+        this.other = this.leader ? '#00688B' : '#CD0000';
+        this.turn = this.leader;
+        if (this.turn) {
+            this.status('It is your turn.');
+        } else {
+            this.status('Waiting on other player.');
+        }
+        this.createNewBoard();
+        this.$el.fadeIn();
+    },
+    createNewBoard: function() {
+        var i = 0, box;
+        this.board = $('#board', this.$el);
+        this.board.html('');
+        for (i = 0; i < 9; i++) {
+            this.board.append($('<div>', {'id': 'box-' + i + 1}).addClass('box'));
+        }
+    },
+    gotMessage: function (e) {
+        var data = e.data, box;
+        console.log("Got game message " + data);
+        if (!this.turn) {
+            box = $('#' + data);
+            if (!box.text()) {
+                box.css('background-color', this.other);
+                this.turn = true;
+                this.status('It is your turn.');
+            }
+        }
+    },
+    send: function(message) {
+        this.channel.send(message);
+    },
+    pickBox: function (e) {
+        var box;
+        e.preventDefault();
+        if (this.turn) {
+            box = $(e.target);
+            if (!box.text()) {
+                box.css('background-color', this.piece);
+                this.turn = false;
+                this.status('Waiting on other player.');
+                this.send(box.attr('id'));
+            }            
+        }
+    },
+    status: function (message) {
+        $('.status', this.$el).html(message);
+    }
+});
+
 var AppRouter = Backbone.Router.extend({
     routes: {
         "": "homePage",
         "offer-:offer": "acceptOffer",
         "answer-:answer": "acceptAnswer",
-        "bin-:id": "viewBin"
+        "game-:id": "viewGame"
     },
     initialize: function () {
         this.sender = new RTCPeerConnection(servers, config);
@@ -15,19 +76,21 @@ var AppRouter = Backbone.Router.extend({
         this.channel = null;
         this.localOffer = null; 
         this.remoteOfffer = null;
-        this.bin = null;
+        this.game_id = null;
+        this.game = null;
+        this.leader = true;
         getUserMedia({audio:true, video:false, fake: true},
             _.bind(this.gotLocalStream, this), _.bind(this.reportFailure, this));
     },
     homePage: function () {
         var hash = window.btoa(JSON.stringify(this.localOffer));
-        $('#home #status').text('Creating a new bin...');
+        $('#home .status').text('Creating a new game...');
         $('#offer').attr('href', window.location.href + '#offer-' + hash);
         $('#offer').show();
-        this.navigate('bin-' + Math.random().toString(36).substring(7), {trigger: true});
     },
     acceptOffer: function (offer) {
-        $('#home #status').text('Joining an existing bin...');
+        this.leader = false;
+        $('#home .status').text('Joining an existing game...');
         this.receiver.onaddstream = _.bind(this.gotRemoteStream, this);
         this.receiver.ondatachannel = _.bind(this.gotDataChannel, this);
         this.remoteOfffer = new RTCSessionDescription(JSON.parse(window.atob(offer)));
@@ -41,17 +104,17 @@ var AppRouter = Backbone.Router.extend({
         this.sender.setRemoteDescription(this.remoteOfffer);
         this.sender.onicecandidate = _.bind(this.addIceCandidate, this, this.receiver);
         $('#offer').hide();
-        this.navigate('bin-' + this.bin, {trigger: true});
+        $('#home .status').text('Establishing remote connection...');
     },
-    viewBin: function (id) {
-        this.bin = id;
+    viewGame: function (id) {
+        this.game_id = id;
         $('#home').hide();
-        $('#bin').fadeIn();
+        this.game = new GameView({leader: this.leader, channel: this.channel});
     },
     gotLocalStream: function (stream) {
         console.log("Got local stream.");
         this.sender.addStream(stream);
-        this.channel = this.sender.createDataChannel('bin', {reliable:false});
+        this.channel = this.sender.createDataChannel('game', {reliable:false});
         this.channel.onmessage = _.bind(this.gotMessage, this);
         this.sender.createOffer(_.bind(this.gotLocalOffer, this), _.bind(this.reportFailure, this));
     },
@@ -90,18 +153,17 @@ var AppRouter = Backbone.Router.extend({
     gotMessage: function (e) {
         var data = e.data
         console.log("Got message", data);
-        if (!this.bin) {
-            // First message when the peer connects in the bin id
-            this.bin = data;
-            this.navigate('bin-' + this.bin, {trigger: true});
+        if (!this.game) {
+            // First message when the peer connects in the game id
+            this.game_id = data;
+            this.navigate('game-' + this.game_id, {trigger: true});
         }
     },
     gotNewConnection: function (e) {
         console.log('New connection');
-        this.send(this.bin);
-    },
-    send: function(message) {
-        this.channel.send(message);
+        this.game_id = Math.random().toString(36).substring(7);
+        this.channel.send(this.game_id);
+        this.navigate('game-' + this.game_id, {trigger: true});
     }
 });
 
